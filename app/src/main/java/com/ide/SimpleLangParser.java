9345eb26a0a3730259dc19057a6c4961b4995b9d
Nothing <155074292+MRNobodyFA@users.noreply.github.com>
2024-11-20 21:14:33 +0330

    Create SimpleLangParser.java

---
+++ app/src/main/java/com/ide/SimpleLangParser.java
@@ -0,0 +1,17 @@
+// Generated from SimpleLang.g4 by ANTLR 4.9.2
+package com.example.advancedide;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class SimpleLangParser extends Parser {
+    // Parser rules and members
+
+    public static class ProgContext extends ParserRuleContext {
+        public List<StatContext> stat() {
+            return getRuleContexts(StatContext.class);
+        }
+        // other methods and members
+    }
+
+    // Other parser rule contexts and methods
+}
