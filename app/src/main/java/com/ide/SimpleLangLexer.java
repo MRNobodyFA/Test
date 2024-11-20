3655a667dc23b0e7e678c115e4dcdc37054a33fd
Nothing <155074292+MRNobodyFA@users.noreply.github.com>
2024-11-20 21:14:04 +0330

    Create SimpleLangLexer.java

---
+++ app/src/main/java/com/ide/SimpleLangLexer.java
@@ -0,0 +1,11 @@
+// Generated from SimpleLang.g4 by ANTLR 4.9.2
+package com.example.advancedide;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+
+public class SimpleLangLexer extends Lexer {
+    // Lexer rules and members
+}
