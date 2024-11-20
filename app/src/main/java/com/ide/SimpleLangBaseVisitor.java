6fe73c13aa895bb689116eb196b4a8b0f93863bc
Nothing <155074292+MRNobodyFA@users.noreply.github.com>
2024-11-20 21:14:58 +0330

    Create SimpleLangBaseVisitor.java

---
+++ app/src/main/java/com/ide/SimpleLangBaseVisitor.java
@@ -0,0 +1,7 @@
+// Generated from SimpleLang.g4 by ANTLR 4.9.2
+package com.example.advancedide;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+public class SimpleLangBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements SimpleLangVisitor<T> {
+    // Visitor methods
+}
